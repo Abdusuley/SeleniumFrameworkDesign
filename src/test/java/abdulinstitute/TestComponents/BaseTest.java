4023f8d3effb098a99d291f2d40a27e73c7c373e
abdul <abdusuley01@gmail.com>
2025-12-25 14:14:05 -0800

    my changes

---
+++ src/test/java/abdulinstitute/TestComponents/BaseTest.java
@@ -0,0 +1,112 @@
+package abdulinstitute.TestComponents;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import abdulinstitute.pageobjects.LandingPage;
+
+public class BaseTest {
+
+    public WebDriver driver;
+    public LandingPage landingPage;
+
+    public WebDriver initializeDriver() throws IOException {
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
+                + "//src//main//java//abdulinstitute//resources//GlobalData.properties");
+        prop.load(fis);
+
+        String browserName = System.getProperty("browser") != null ?
+                System.getProperty("browser") : prop.getProperty("browser");
+
+        switch (browserName.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                driver = new ChromeDriver(chromeOptions);
+                break;
+
+            case "headlesschrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions headlessOptions = new ChromeOptions();
+                headlessOptions.addArguments("--headless=new");
+                headlessOptions.addArguments("--no-sandbox");
+                headlessOptions.addArguments("--disable-dev-shm-usage");
+                headlessOptions.addArguments("--window-size=1440,900");
+                driver = new ChromeDriver(headlessOptions);
+                break;
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browserName);
+        }
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException {
+        String jsonContent = FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {});
+    }
+
+    public String getScreenshot(String testCaseName, WebDriver driver) throws IOException {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        File file = new File(System.getProperty("user.dir") + "//reports//" + testCaseName + ".png");
+        FileUtils.copyFile(source, file);
+        return System.getProperty("user.dir") + "//reports//" + testCaseName + ".png";
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public LandingPage launchApplication() throws IOException {
+        driver = initializeDriver();
+        landingPage = new LandingPage(driver);
+        landingPage.goTo();
+        return landingPage;
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit(); // Use quit() instead of close() for better cleanup
+        }
+    }
+}
---
+++ src/test/java/abdulinstitute/tests/ErrorValidationsTest.java
@@ -0,0 +1,50 @@
+package abdulinstitute.tests;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import abdulinstitute.TestComponents.BaseTest;
+import abdulinstitute.TestComponents.Retry;
+import abdulinstitute.pageobjects.CartPage;
+import abdulinstitute.pageobjects.CheckoutPage;
+import abdulinstitute.pageobjects.ConfirmationPage;
+import abdulinstitute.pageobjects.ProductCatalogue;
+
+public class ErrorValidationsTest extends BaseTest {
+
+	@Test(groups= {"ErrorHandling"},retryAnalyzer=Retry.class)
+	public void LoginErrorValidation() throws IOException, InterruptedException {
+
+	
+		landingPage.loginApplication("automate@gmail.com", "Iamki000");
+		Assert.assertEquals(landingPage.getErrorMessage(), "Incorrect email or password.");
+
+	}
+	
+
+	@Test
+	public void ProductErrorValidation() throws IOException, InterruptedException
+	{
+
+		String productName = "ZARA COAT 3";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("abdul.salih@outlook.com", "Deen2024");
+		List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(productName);
+		CartPage cartPage = productCatalogue.goToCartPage();
+		Boolean match = cartPage.VerifyProductDisplay("ZARA COAT 33");
+		Assert.assertFalse(match);
+		
+	
+
+	}
+
+	
+	
+
+}
